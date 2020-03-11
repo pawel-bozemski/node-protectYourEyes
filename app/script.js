0792324f31c746e8527e9ea482a8102b02d1695a
Pawel Bozemski <paw.boz@gmail.com>
2020-03-11 16:48:35 +0100

    Add state and determination of displaying elements

--- app/script.js
+++ app/script.js
@@ -3,19 +3,25 @@ import { render } from 'react-dom';
 
 class App extends React.Component {
 
+  state = {
+    status: 'off',
+    time: '00:05',
+    timer: null,
+  }
+
   render() {
+    const { status } = this.state;
     return (
       <div>
         <h1>Protect your eyes</h1>
         <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
         <p>This app will help you track your time and inform you when it's time to rest.</p>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />
-        <div className="timer">
-          18:23
-        </div>
-        <button className="btn">Start</button>
-        <button className="btn">Stop</button>
+        {(status === 'off')}
+        {(status === 'work') && <img src="./images/work.png" />}
+        {(status === 'rest') && <img src="./images/rest.png" />}
+        {(status !== 'off') && <div className="timer">18:23</div>}
+        {(status === 'off') && <button className="btn">Start</button>}
+        {(status !== 'off') && <button className="btn">Stop</button>}
         <button className="btn btn-close">X</button>
       </div>
     )
